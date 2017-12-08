bffeabdb8439fb87173b979726e58bc6b2c3f7eb
beefchimi <curtdulmage@gmail.com>
2017-12-08 16:39:37 -0500

    first pass at Collidable example

--- examples/src/scripts/app.js
+++ examples/src/scripts/app.js
@@ -9,6 +9,7 @@ import Transformed from '../content/Sortable/Transformed';
 import MultipleContainers from '../content/Sortable/MultipleContainers';
 import FloatedLayout from '../content/Swappable/FloatedLayout';
 import GridLayout from '../content/Swappable/GridLayout';
+import Collidable from '../content/Plugins/Collidable';
 import SwapAnimation from '../content/Plugins/SwapAnimation';
 
 const navActivator = document.getElementById('MobileNavActivator');
@@ -26,5 +27,6 @@ SimpleList();
 Transformed();
 MultipleContainers();
 FloatedLayout();
-SwapAnimation();
 GridLayout();
+Collidable();
+SwapAnimation();
